8d9e9c1696939165d8a4230abe98aa540ba02e90
Hubert <hubert@relikowski.dev>
2024-10-24 20:51:36 +0200

    Add list with events plane

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import "react-calendar/dist/Calendar.css";
 import Calendar from "react-calendar";
+import { events } from "@/lib/utils";
+import Image from "next/image";
 
 type ValuePiece = Date | null;
 
@@ -14,6 +16,24 @@ const EventCalendar = () => {
   return (
     <div className="bg-white p-4 rounded-md">
       <Calendar onChange={onChange} value={value} locale="en-EN" />
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <Image src="/moreDark.png" alt="moreDark" width={20} height={20} />
+      </div>
+      <div className="flex flex-col gap-4">
+        {events.map(({ id, title, time, description }) => (
+          <div
+            key={id}
+            className="p-5 rounded-md border-2 border-t-4 border-gray-100 odd:border-t-sky even:border-t-yellow"
+          >
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold text-gray-600">{title}</h1>
+              <span className="text-gray-300 text-xs">{time}</span>
+            </div>
+            <p className="mt-2 text-gray-400 text-sm">{description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -216,3 +216,24 @@ export const dataFinanseChart = [
     expense: 6553,
   },
 ];
+
+export const events = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
