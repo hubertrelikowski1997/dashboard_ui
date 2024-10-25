b50b39f14627018d849aeeb8b1b92194b55c87d1
Hubert <hubert@relikowski.dev>
2024-10-25 12:06:29 +0200

    UI version finish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
+  title: "SchoolDash",
   description: "Next.js School Management System",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="w-full flex flex-col items-center p-10">
+      <p>page under construction but you can enter dashboard as admin !</p>
+      <a href="/admin" className="underline text-xl uppercase text-red-500">
+        admin
+      </a>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
